106c96cf788b492d10b32b774b69b666c5bdea8a
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-09-01 19:44:55 +0000

    1. Forçar renderização só no cliente para componentes problemáticos
    Par

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import ClientOnly from '@/components/client-only';
 
 export const metadata: Metadata = {
   title: 'Corridinha GT',
@@ -21,7 +22,9 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
-        <Toaster />
+        <ClientOnly>
+          <Toaster />
+        </ClientOnly>
       </body>
     </html>
   );
