f99a83930a3f87510aea108b4f3c4913d230d3af
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-09-09 13:07:59 +0000

    fonte padrao monteserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Inter:wght@400;500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
       </head>
