39d548b9d6577534d0d57b5ae92196b330a25122
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-09-15 11:28:54 +0000

    apesar de aparentemente as paginas estão funcionando, ainda existe diver

--- src/app/dashboard/[storeId]/page.tsx
+++ src/app/dashboard/[storeId]/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { GoalGetterDashboard } from "@/components/goal-getter-dashboard";
--- src/components/seller-tab.tsx
+++ src/components/seller-tab.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { Seller, Goals, Incentives } from "@/lib/storage";
