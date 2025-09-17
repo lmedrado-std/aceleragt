e03492835944d43538898c5a24eb3aa71a5704c9
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-09-17 23:31:01 +0000

    barra lateral nao retornou cor original

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -72,11 +72,11 @@ export function Sidebar() {
   const isAdminPage = pathname.startsWith('/admin');
 
   return (
-    <aside className="flex h-full max-h-screen flex-col gap-2 bg-muted/40">
+    <aside className="flex h-full max-h-screen flex-col gap-2">
       <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-         <Link href="/">
-            <Logo />
-         </Link>
+        <Link href="/">
+          <Logo />
+        </Link>
       </div>
 
       <nav className="flex-1 p-2 space-y-1 overflow-y-auto">
