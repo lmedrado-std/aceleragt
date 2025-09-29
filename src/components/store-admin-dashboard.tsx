8299a5795f8ce57b761127bf994aa4a2f87f04f1
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-09-29 12:09:45 +0000

    é necessario a dica ficar no topo ou abir um pop up para nao atrapalhar

--- src/components/store-admin-dashboard.tsx
+++ src/components/store-admin-dashboard.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
@@ -126,6 +125,14 @@ export function StoreAdminDashboard({ sellers, goals, incentives }: StoreAdminDa
       </CardHeader>
       <CardContent className="space-y-8">
         
+        <Alert>
+          <Megaphone className="h-4 w-4" />
+          <AlertTitle>Dica de Gestão!</AlertTitle>
+          <AlertDescription>
+            Manter os lançamentos da equipe sempre atualizados deixará todos motivados com seus resultados. É importante fazer sempre o alinhamento do desempenho com a equipe!
+          </AlertDescription>
+        </Alert>
+
         {/* === KPIs === */}
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
             <InfoCard 
@@ -238,14 +245,6 @@ export function StoreAdminDashboard({ sellers, goals, incentives }: StoreAdminDa
             </Card>
         </div>
         
-        <Alert>
-          <Megaphone className="h-4 w-4" />
-          <AlertTitle>Dica de Gestão!</AlertTitle>
-          <AlertDescription>
-            Manter os lançamentos da equipe sempre atualizados deixará todos motivados com seus resultados. É importante fazer sempre o alinhamento do desempenho com a equipe!
-          </AlertDescription>
-        </Alert>
-
       </CardContent>
     </Card>
   );
