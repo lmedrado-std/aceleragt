2a9a4f5ad55cb718b507f6cbd40ae907e10adab8
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-09-29 16:59:45 +0000

    o menu selecionado poderia ficar com alguma animação para identificar qu

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -32,7 +32,7 @@ const TabsTrigger = React.forwardRef<
     className={cn(
       "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
       "border border-input bg-background text-foreground hover:bg-accent hover:text-accent-foreground",
-      "data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow-sm data-[state=active]:border-transparent",
+      "data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow-sm data-[state=active]:border-transparent data-[state=active]:animate-subtle-pulse",
       className
     )}
     {...props}
