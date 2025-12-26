928e36a7be81e40914f1f3fbab2acc1ac1bc782d
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-12-26 20:34:11 +0000

    agora somente adicionar acima de cada estagio a meta atingida

--- src/components/progress-display.tsx
+++ src/components/progress-display.tsx
@@ -77,6 +77,7 @@ const SegmentedProgressBar = ({
               <Tooltip>
                 <TooltipTrigger asChild>
                   <div className="relative flex-1 h-full bg-white/20 rounded-md overflow-hidden">
+                     <p className="absolute text-white/80 text-[10px] left-1/2 -translate-x-1/2 top-[-18px] font-bold">{formatter(g.value)}</p>
                      <div
                         className={cn("h-full rounded-md", segmentColors[i % segmentColors.length])}
                         style={{ width: `${fillPercentage}%` }}
