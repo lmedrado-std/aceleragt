3475a031f835c2ebc29c26b27958f0edf27b6638
Luciano Medrado <lucianomedrado2012@gmail.com>
2025-12-29 20:56:39 +0000

    V 2.0 - bUILD eSTÁVEL (_for element src/app/page.tsx > p_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ function AppFooter() {
         <footer className="mt-auto pt-16 pb-8 text-center text-slate-400">
             {renderThemeToggle()}
             <div className="mt-4 space-y-1 text-sm">
-                <p>v1.0.1 - Build Estável</p>
+                <p>V 2.0 - bUILD eSTÁVEL</p>
                 <p>RyannBreston desenvolvedor</p>
                 <p>© {new Date().getFullYear()} Acelera GT.</p>
             </div>
