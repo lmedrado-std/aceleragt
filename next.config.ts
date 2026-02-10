55c98921c97501305ff0368bd882fcf8c6d115e1
Luciano Medrado <lucianomedrado2012@gmail.com>
2026-02-10 21:45:22 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://picsum.

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
